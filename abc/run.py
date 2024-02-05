cceb7a9cad394a5db1817200a98ea4f8ec7b6566
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-02-05 10:50:29 +0000

    implemented abc for mock data

--- abc/run.py
+++ abc/run.py
@@ -1,12 +1,12 @@
 import random
+import copy
+
 _range_ = 100
 hidden_layer_sizes = [x * 10 for x in range(1, _range_ + 1)]
 number_of_hidden_layers = [x for x in range(1, _range_ + 1)]
 learning_rates = [x/1000 for x in range(1, _range_ + 1)]
-particle_nums = 100
 data_param_to_metric = {}
 data_metric_to_param = {}
-
 def generate_synthetic_data():
     global data_param_to_metric
     global data_metric_to_param
@@ -38,4 +38,118 @@ def generate_synthetic_data():
                     max_mem = memory
     print('min-accuracy:', round(min, 2), 'max-accuracy:', round(max, 2), 'min-inference-time:', round(min_time, 2),'max-inference-time:', round(max_time, 2),'min-memory:', round(min_mem, 2), 'max-memory:', round(max_mem, 2))
 
-generate_synthetic_data()
+# abc parameters
+dimensions = 3                                              # number of parameters
+food_sources_nums = 10
+particle_nums = dimensions * food_sources_nums              # multiple of 3 because we have three parameters to be tuned (N)
+iteration_nums = 30                                       # you can set any number
+abandoned_solution_limit = 1
+
+def calculate_fitness(x):
+    return ((1/(1 + x)) if x > 0 else (1 + abs(x))) 
+
+def objective_function(x):
+    accuracy, time, memory = data_param_to_metric[(x[0] * 10, x[1], x[2]/1000)]
+    return  accuracy / (time * memory)
+
+def perform_employed_phase(food_sources_data, f_x, fitnesses, trials):
+    for i in range(food_sources_nums):
+        X = food_sources_data[i]
+        choices_idx = [j for j in range(food_sources_nums)]
+        choices_idx.remove(i)
+        Xp = food_sources_data[random.choice(choices_idx)]
+        dim_idx_choice = random.choice([j for j in range(dimensions)])
+        phi = random.uniform(1/_range_, _range_/_range_)
+        x_new = X[dim_idx_choice] + phi * (X[dim_idx_choice] - Xp[dim_idx_choice])
+        x_new = round(x_new)
+        if x_new < 1: 
+            x_new = 1
+        if x_new > _range_: 
+            x_new = _range_
+        X_new = copy.deepcopy(X)
+        X_new[dim_idx_choice] = x_new
+        new_output = objective_function(X_new)
+        new_fitness = calculate_fitness(new_output)
+        if fitnesses[i] > new_fitness:
+            fitnesses[i] = new_fitness
+            trials[i] = 0
+            f_x[i] = new_output
+            food_sources_data[i] = X_new
+        else: 
+            trials[i] += trials[i]
+
+def perform_onlooker_phase(food_sources_data, f_x, fitnesses, trials):
+    i = 0                                   # index of current food source
+    k = 0                                   # number of iteration
+    sum_of_fitnessess = sum(fitnesses)
+    probabilities = [fitness/sum_of_fitnessess for fitness in fitnesses]
+    while i < food_sources_nums:
+        r = random.random()
+        if r < probabilities[i]: 
+            X = food_sources_data[i]
+            choices_idx = [j for j in range(food_sources_nums)]
+            choices_idx.remove(i)
+            Xp = food_sources_data[random.choice(choices_idx)]
+            dim_idx_choice = random.choice([j for j in range(dimensions)])
+            phi = random.uniform(1/_range_, _range_/_range_)
+            x_new = X[dim_idx_choice] + phi * (X[dim_idx_choice] - Xp[dim_idx_choice])
+            x_new = round(x_new)
+            if x_new < 1: 
+                x_new = 1
+            if x_new > _range_: 
+                x_new = _range_
+            X_new = copy.deepcopy(X)
+            X_new[dim_idx_choice] = x_new
+            new_output = objective_function(X_new)
+            new_fitness = calculate_fitness(new_output)
+            if fitnesses[i] > new_fitness:
+                fitnesses[i] = new_fitness
+                trials[i] = 0
+                f_x[i] = new_output
+                food_sources_data[i] = X_new
+            else: 
+                trials[i] += trials[i]
+            i += 1
+        k += 1
+        k %= food_sources_nums
+        
+def perform_scout_phase(food_sources_data, f_x, fitnesses, trials):
+    for i in range(food_sources_nums):
+        if trials[i] > abandoned_solution_limit:
+            food_sources_data[i] = [random.randint(1, _range_) for _ in range(dimensions)]
+            f_x[i] = objective_function(food_sources_data[i])
+            fitnesses[i] = calculate_fitness(f_x[i])
+            trials[i] = 0
+
+
+def run_abc(): 
+    food_sources_data = [[random.randint(1, _range_) for i in range(dimensions)] for _ in range(food_sources_nums)]
+    f_x = [objective_function(x) for x in food_sources_data]            # maximize
+    fitnesses = [calculate_fitness(x) for x in f_x]                     # minimize
+    trials = [0 for _ in range(food_sources_nums)]
+    for _ in range(iteration_nums):
+        perform_employed_phase(food_sources_data, f_x, fitnesses, trials)
+        perform_onlooker_phase(food_sources_data, f_x, fitnesses, trials)
+        perform_scout_phase(food_sources_data, f_x, fitnesses, trials)
+    
+    max_idx = 0
+    max_fx = f_x[0]
+    for i in range(food_sources_nums):
+        if max_fx < f_x[i]:
+            max_fx = f_x[i]
+            max_idx = i
+    
+    return food_sources_data[max_idx] 
+
+if __name__ == "__main__":
+    generate_synthetic_data()
+    best_position = run_abc()
+    accuracy, time, memory = data_param_to_metric[(best_position[0] * 10, best_position[1], best_position[2]/1000)]
+    
+    print("Optimal Accuracy: ", round(accuracy, 3), "%")
+    print("Optimal inference time: ", round(time, 3), "s")
+    print("Optimal required memory: ", round(memory, 3), "MiB")
+    print("FOR")
+    print("hidden_layer_size: ", best_position[0] * 10)
+    print("num_hidden_layer: ", best_position[1])
+    print("learning_rate: ", best_position[2] / 1000)
